0a8ed9e08769671bc73aa0042cd60dc53437a3c7
ned <nedschultz@outlook.com>
2023-01-17 13:57:43 +1000

    commit tree 240bb8079445ae01443d33a4bbdb219ffa75d5ad

--- index.js
+++ index.js
@@ -9,6 +9,69 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord(employeePropsAraay) {
+    return {
+        firstName: employeePropsAraay[0],
+        familyName: employeePropsAraay[1],
+        title: employeePropsAraay[2],
+        payPerHour: employeePropsAraay[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+}
+
+function createEmployeeRecords(arrayOfEmployeePropsArray) {
+    return arrayOfEmployeePropsArray.map(createEmployeeRecord)
+};
+
+function createTimeInEvent(date) {
+    let timeInEventObj = {
+        type: "TimeIn",
+        date: date.split(" ")[0],
+        hour: parseInt(date.split(" ")[1]),
+    };
+
+    this.timeInEvents.push(timeInEventObj);
+    return this;
+}
+
+
+function createTimeOutEvent(date) {
+    let timeOutEventObj = {
+        type: "TimeOut",
+        date: date.split(" ")[0],
+        hour: parseInt(date.split(" ")[1]),
+    };
+
+    this.timeOutEvents.push(timeOutEventObj);
+    return this;
+}
+
+function hoursWorkedOnDate(employeeObj, date) {
+    let timeInEventOnDate = employeeObj.timeInEvents.find(
+        (timeInEvent) => timeInEvent.date === date
+    );
+
+    let timOutEventOnDate = employeeObj.timeOutEvents.find(
+        (timeOutEvent) => timeOutEvent.date === date
+    );
+
+    return (timOutEventOnDate.hour - timeInEventOnDate.hour) / 100;
+}
+
+function wagesEarnedOnDate(employeeObj, date) {
+    return hoursWorkedOnDate(employeeObj, date) * employeeObj.payPerHour;
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords
+        .map(allWagesFor)
+        .reduce(
+            (partialSumOfAllWages, currentEmployeeWage) =>
+                partialSumOfAllWages + currentEmployeeWage
+        );
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
